ab3f0517ead9b5f1d93ab3319058f975212fe420
Aman Burman <amanb21199@gmail.com>
2025-03-08 15:01:28 +0530

    Add NewTodoPage and Redirection

--- basic-web-app/src/main/java/com/springboot/basic_web_app/todoapp/TodoController.java
+++ basic-web-app/src/main/java/com/springboot/basic_web_app/todoapp/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -19,9 +20,21 @@ public class TodoController {
 	}
 
 	@RequestMapping("/list-todos")
-	public String l(ModelMap model) {
+	public String listTodos(ModelMap model) {
 		List<Todo> todos = todoService.findByUsername("Aman");
 		model.put("todos",todos);
 		return "listTodos";
+	}
+	
+	@RequestMapping(value="/add-todo",method = RequestMethod.GET)
+	public String showNewTodoPage() {
+		return "todo";
+	}
+	
+	@RequestMapping(value="/add-todo",method = RequestMethod.POST)
+	public String addNewTodoPage() {
+		// Instead re-writing listTodos() block here again and calling jsp,
+		// we can just redirect to the url using below.
+		return "redirect:list-todos";
 	}
 }
